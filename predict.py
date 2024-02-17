fed1a667a8b3de0b602ba75204c4d8f9be235a95
dschnell <dschnell@grammatek.com>
2024-02-17 20:46:54 +0000

    Update requirements.txt for newer pytorch/torchaudio - tested with Python 3.10; added filenames for batch-mode/directory mode via inp_directory. Also added sorting for filenames and scores via cmd line parameters

--- predict.py
+++ predict.py
@@ -6,6 +6,7 @@ import torchaudio
 from score import Score
 import torch
 
+
 def get_arg():
     parser = argparse.ArgumentParser()
     parser.add_argument("--bs", required=False, default=None, type=int)
@@ -15,10 +16,12 @@ def get_arg():
     parser.add_argument("--inp_path", required=False, default=None, type=pathlib.Path)
     parser.add_argument("--out_path", required=True, type=pathlib.Path)
     parser.add_argument("--num_workers", required=False, default=0, type=int)
+    parser.add_argument("--sort_by", required=False, default=None, choices=["filename", "score"],
+                        help="Sort output by 'filename' or 'score'")
     return parser.parse_args()
 
 
-class Dataset(Dataset):
+class AudioDataset(Dataset):
     def __init__(self, dir_path: pathlib.Path):
         self.wavlist = list(dir_path.glob("*.wav"))
         _, self.sr = torchaudio.load(self.wavlist[0])
@@ -30,19 +33,22 @@ class Dataset(Dataset):
         fname = self.wavlist[idx]
         wav, _ = torchaudio.load(fname)
         sample = {
-            "wav": wav}
+            "wav": wav,
+            "fname": fname.name
+        }
         return sample
-    
+
     def collate_fn(self, batch):
         max_len = max([x["wav"].shape[1] for x in batch])
         out = []
-        # Performing repeat padding
+        fnames = []
         for t in batch:
             wav = t["wav"]
+            fnames.append(t["fname"])  # Fügt den Dateinamen hinzu
             amount_to_pad = max_len - wav.shape[1]
-            padding_tensor = wav.repeat(1,1+amount_to_pad//wav.size(1))
-            out.append(torch.cat((wav,padding_tensor[:,:amount_to_pad]),dim=1))
-        return torch.stack(out, dim=0)
+            padding_tensor = wav.repeat(1, 1 + amount_to_pad // wav.size(1))
+            out.append(torch.cat((wav, padding_tensor[:, :amount_to_pad]), dim=1))
+        return {"wav": torch.stack(out, dim=0), "fname": fnames}  # Gibt ein Dict zurück
 
 
 def main():
@@ -57,29 +63,37 @@ def main():
         scorer = Score(ckpt_path=args.ckpt_path, input_sample_rate=sr, device=device)
         score = scorer.score(wav.to(device))
         with open(args.out_path, "w") as fw:
-            fw.write(str(score[0]))
+            fw.write(f"{args.inp_path.name}: {score[0]}\n")
     else:
         assert args.inp_dir is not None, "inp_dir is required when mode is predict_dir."
         assert args.bs is not None, "bs is required when mode is predict_dir."
         assert args.inp_path is None, "inp_path should be None."
         assert args.inp_dir.exists()
         assert args.inp_dir.is_dir()
-        dataset = Dataset(dir_path=args.inp_dir)
+        dataset = AudioDataset(dir_path=args.inp_dir)
         loader = DataLoader(
             dataset,
             batch_size=args.bs,
             collate_fn=dataset.collate_fn,
-            shuffle=True,
+            shuffle=False,
             num_workers=args.num_workers)
         sr = dataset.sr
         scorer = Score(ckpt_path=args.ckpt_path, input_sample_rate=sr, device=device)
-        with open(args.out_path, 'w'):
-            pass
+        results = []
         for batch in tqdm.tqdm(loader):
-            scores = scorer.score(batch.to(device))
-            with open(args.out_path, 'a') as fw:
-                for s in scores:
-                    fw.write(str(s) + "\n")
+            scores = scorer.score(batch["wav"].to(device))
+            for fname, s in zip(batch["fname"], scores):
+                results.append((fname, s.item()))
+
+        # Sort results according to commandline parameters
+        if args.sort_by == "filename":
+            results.sort(key=lambda x: x[0])
+        elif args.sort_by == "score":
+            results.sort(key=lambda x: x[1], reverse=True)
+
+        with open(args.out_path, 'w') as fw:
+            for fname, s in results:
+                fw.write(f"{fname}: {s}\n")
 
 
 if __name__ == '__main__':
